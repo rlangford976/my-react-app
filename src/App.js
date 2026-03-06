9cd4219690cd22815b1a43fdc6d5371bf8876eb5
Priya <rlangford976@gmail.com>
2026-03-06 11:29:59 +0530

    just testing these

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+const redux = require('redux');
+const createStore=redux.createStore;
+const BUY_CAKE='BUY_CAKE';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function buyCake(){
+  return{
+  type:BUY_CAKE
+  }
 }
 
-export default App;
+const initialState={
+  numOfCakes: 10
+}
+//(prevstate,action)=>newState
+const reducer = (state=initialState,action)=>{
+ switch (action.type){
+case BUY_CAKE :return{
+  ...state,
+  numOfCakes :state.numOfCakes-1
+}
+default: return state 
+ }
+}
+const store =createStore(reducer);
+console.log("InitialState",store.getState());

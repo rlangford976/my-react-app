58b26eed1040118f3464d7491be31cd0e9389c84
Priya <rlangford976@gmail.com>
2026-04-24 16:31:41 +0530

    save changes

--- src/App.js
+++ src/App.js
@@ -4,10 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
-            <h1>Hello World</h1>
-      <h1>Hello World</h1>
-
+      <h1>Live Chat Testing</h1>
     </div>
   );
 }

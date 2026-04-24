0546d6184f2f851fbb8f5a20b4a2686eef344f5f
Priya <rlangford976@gmail.com>
2026-04-24 16:26:31 +0530

    Ready for Vercel deploy

--- src/App.js
+++ src/App.js
@@ -5,11 +5,7 @@ function App() {
   return (
     <div className="App">
       <h1>Hello World</h1>
-            <h1>Hello World</h1>
-      <h1>Hello World</h1>
-
-            <h1>You are the best</h1>
-
+  
 
     </div>
   );
